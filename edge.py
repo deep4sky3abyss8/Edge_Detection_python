4d0553a90bf0a536c36fa809269b637274b14b03
USERNAME <mmdmahdiqasemisa@gmail.com>
2025-01-27 20:06:45 +0330

    ist gray now

--- edge.py
+++ edge.py
@@ -10,13 +10,19 @@ img = cv2.imread('F:/univercity/projects/dmath/hh/hh.JPG')
 height , width , channels = img.shape
 
 #--------gray color filter box------------------------------
-edges = filters.sobel(img)  
-plt.imshow(edges, cmap='gray')
+grayimg=np.zeros((height,width),dtype=np.uint8)
 
+for i in range(height):
+    for j in range(width):
+        b,g,r = img[i,j]
+        graying=int(0.2989*r + 0.5870*g + 0.1140*b)
+        grayimg[i,j]=graying
 
+#-------------edge finding box------------------------------
+#edges = filters.sobel(img)
+#plt.imshow(edges, cmap='gray')
 
-
-
-#----------output box------------------
-cv2.imshow('Image',img)
-
+#----------output box---------------------------------------
+cv2.imshow('grayImage',grayimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows
