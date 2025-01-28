bdd5f0726672f82845c58792db10d10808a79b00
USERNAME <mmdmahdiqasemisa@gmail.com>
2025-01-28 20:52:31 +0330

    sobel filter to find edges

--- edge.py
+++ edge.py
@@ -1,4 +1,4 @@
-#-------------library box-----------------------------------
+#----------------------library box---------------------------------
 
 import cv2
 import numpy as np
@@ -9,7 +9,7 @@ from skimage import io
 from skimage.filters import gaussian
 
 
-#----------------------function box----------------------------
+#----------------------function box--------------------------------
 
 #def gradian(i,j,matrix):                                             # function for sobel filter write by myself
     #Gx = 2*(matrix[i,j+1]-matrix[i,j-1]) + (matrix[i-1,j+1]+matrix[i+1,j+1]-matrix[i-1,j-1]-matrix[i+1,j-1])
@@ -17,13 +17,15 @@ from skimage.filters import gaussian
     #return int(((Gx)**2 +(Gy)**2 )**0.5)
 #                                                here we calculate gradiant of 3x3 matrix which central object [i,j]
 
-#--------img input box--------------------------------------
+#------------------------img input box------------------------------
 
-img = cv2.imread('F:/univercity/projects/dmath/hh/hh.JPG')
+#img = cv2.imread('F:/univercity/projects/dmath/hh/hh.JPG')
+img = cv2.imread('F:/univercity/projects/dmath/hh/hhh.jpg')
+
 height , width , channels = img.shape                              # for loops need this line
 
 
-#--------gray color filter box------------------------------
+#----------------------gray color filter box-------------------------
 
 grayimg=np.zeros((height,width),dtype=np.uint8)                   # that's a new img in size of our original img 
 
@@ -34,22 +36,18 @@ for i in range(height):
         grayimg[i,j]=grayval                                      # already we can apply this all on original img but we do it to check changes by steps
 
 
-#---------------gowse filter to reduce noise box------------
-
-guimg= gaussian(grayimg,sigma=5)
-
-#---------------sobel filter box----------------------------
+#--------------------edge finding sobel filter box------------------
 
 sobimg=np.zeros((height,width),dtype=np.uint8)
 
 #+++++++++++++++++++++++++++++
-sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])         # sobel array to product matrises
 sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 #+++++++++++++++++++++++++++++
 
 for i in range(1,height-1):
     for j in range(1,width-1):
-        #sobimg[i,j] = gradian(i,j,grayimg)   # we can alse use this way but its dont work good
+        #sobimg[i,j] = gradian(i,j,grayimg)   # we can alse use this way but its not best way
         
         #+++++++++++++++++++++++++++++++++
         region = grayimg[i-1:i+2, j-1:j+2]
@@ -61,11 +59,17 @@ for i in range(1,height-1):
         #+++++++++++++++++++++++++++++++++
 
 
-#-------------edge finding box------------------------------
-#edges = filters.sobel(img)
-#plt.imshow(edges, cmap='gray')
+#---------------------gowse filter to reduce noise box------------------
 
-#----------output box---------------------------------------
+guimg= gaussian(sobimg,sigma=0.5)
+
+
+#---------------------biggest container finding box----------------------
+
+
+
+#-----------------------output box---------------------------------------
+
 cv2.imshow('orginal',img)
 cv2.imshow('grayImage',grayimg)
 cv2.imshow('bullued',guimg)
