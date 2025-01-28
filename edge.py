25eb1a9fc2c0d9681f1f22c02f0d3bb0c0164c4f
USERNAME <mmdmahdiqasemisa@gmail.com>
2025-01-28 17:03:01 +0330

    gussiean filter had applyed

--- edge.py
+++ edge.py
@@ -3,26 +3,35 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-from skimage import data,filters
+from skimage import io
+from skimage.filters import gaussian
 
 #--------img input box--------------------------------------
 img = cv2.imread('F:/univercity/projects/dmath/hh/hh.JPG')
 height , width , channels = img.shape
 
 #--------gray color filter box------------------------------
-grayimg=np.zeros((height,width),dtype=np.uint8)
+grayimg=np.zeros((height,width),dtype=np.uint8)    # that's a new img in size of our original img 
 
 for i in range(height):
     for j in range(width):
         b,g,r = img[i,j]
-        graying=int(0.2989*r + 0.5870*g + 0.1140*b)
-        grayimg[i,j]=graying
+        grayval=int(0.2989*r + 0.5870*g + 0.1140*b)
+        grayimg[i,j]=grayval   # already we can apply this all on original img but we do it to check changes by steps
+
+#---------------gowse filter to reduce noise box------------
+guimg= gaussian(grayimg,sigma=0.5)
+
+#---------------sobel filter box----------------------------
+#sobelx=cv2.Sobel()
 
 #-------------edge finding box------------------------------
 #edges = filters.sobel(img)
 #plt.imshow(edges, cmap='gray')
 
 #----------output box---------------------------------------
-cv2.imshow('grayImage',grayimg)
+#cv2.imshow('orginal',img)
+#cv2.imshow('grayImage',grayimg)
+cv2.imshow('bullued',guimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows
